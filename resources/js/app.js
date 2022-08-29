66092dffafc8db32a6c1e98e8fdbbb862742f173
Asyraf Hussin <asyrafhussin4@gmail.com>
2022-08-30 02:49:33 +0800

    Add sass boilerplate

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,3 @@
-import './bootstrap';
+import "../sass/app.scss";
+import "popper.js";
+import "bootstrap";
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import sassGlobImports from "vite-plugin-sass-glob-import";
+import path from "path";
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+        sassGlobImports(),
+        laravel(["resources/js/app.js"]),
+        {
+            name: "blade",
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith(".blade.php")) {
+                    server.ws.send({
+                        type: "full-reload",
+                        path: "*",
+                    });
+                }
+            },
+        },
     ],
+    resolve: {
+        alias: {
+            "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+        },
+    },
 });
