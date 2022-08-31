16bb96d4fcc9fd10bc2173014e5ce60e9b0351c6
Asyraf Hussin <asyrafhussin4@gmail.com>
2022-08-31 17:10:10 +0800

    Add TestController

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        $array = array(1,2,3);
+    }
+}
