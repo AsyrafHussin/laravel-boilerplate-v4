2255ad9a9cc96a2eb923b4108f51aa089c76a44a
Asyraf Hussin <asyrafhussin4@gmail.com>
2022-10-07 06:26:48 +0800

    Add log-viewer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        LogViewer::auth(function ($request) {
+            return $request->user()
+                && in_array($request->user()->email, [
+                    // 'admin@domain.com',
+                ]);
+        });
     }
 }
