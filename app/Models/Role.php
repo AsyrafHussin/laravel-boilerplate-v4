fa4406b010a5c9cd34a906631db8fb4f0970fb60
Asyraf Hussin <asyrafhussin4@gmail.com>
2022-10-16 04:08:32 +0800

    Fix role model fillable doc

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -12,7 +12,7 @@ class Role extends Model
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var array<string>
      */
     protected $fillable = ['name', 'title'];
 }
